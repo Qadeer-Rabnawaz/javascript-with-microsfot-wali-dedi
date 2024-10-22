2ab8c7ceea585925a0825be995f58b26fba9386f
Muhammad Qadeer <qadeerrabnawaz1638@gmail.com>
2024-10-22 19:06:11 +0000

    link js file

---
+++ lecture1.js
@@ -0,0 +1,52 @@
+age = 24;
+name = "Qadeer Rabnawaz";
+price = 99.9;
+x = null;
+y = undefined;
+console.log(age);
+console.log(name);
+console.log(price);
+console.log(x);
+console.log(y);
+isfollow = false;
+console.log(isfollow);
+apple = 1;
+Apple = 2;
+console.log(apple);
+console.log(Apple);
+name_full = "Qadeer Rabnawaz";
+Name$full = "Rabnawaz qadeer";
+console.log(name_full);
+console.log(Name$full);
+var apna = "apna college";
+let apna_ = "apna college";
+const _apna = "apna college";
+console.log(apna)
+console.log(apna_)
+console.log(_apna )
+let z = 23;
+
+const student = {
+    fullName: "Qadeer",
+    age: 20,
+    cgpa: 9.0,
+    ispass: true,
+};
+
+console.log(student);
+
+
+const profile = {
+    userName: "sharadhakhapra",
+    isfollow: false,
+    ismessage: false,
+    posts: 195,
+    followes: 569000,
+    following: 4,
+    _profileName: "Shradha Khapra",
+    smallUsername: "@shradhaKhapra",
+    profasion: "Entrepreneur",
+    experencis: "Apna College / Ex-Microsoft,DRDO",
+    lifegole: "to educate is the highest privillege",
+};
+console.log(profile);
