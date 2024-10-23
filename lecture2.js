9234674cf14d94fab379df0ee35e7139747ddae2
Muhammad Qadeer <qadeerrabnawaz1638@gmail.com>
2024-10-23 18:30:59 +0000

    if else statment

--- lecture2.js
+++ lecture2.js
@@ -1,6 +1,6 @@
-let a = 30;
+/*let a = 30;
 let b = 29;
-/*console.log("a + b = ",a + b);
+console.log("a + b = ",a + b);
 console.log("a - b = ",a - b);
 console.log("a / b = ",a / b);
 console.log("a * b = ",a * b);
@@ -33,8 +33,35 @@ console.log("30 !== 30 ",a !== b);
 console.log("30 > 30 ",a > b);
 console.log("30 < 30 ",a < b);
 console.log("30 < 30 ",a <= b);
-console.log("30 > 30 ",a >= b);*/
+console.log("30 > 30 ",a >= b); 
 
 console.log(a > b && a + b == 59);
  console.log(a > b || a == b);
- console.log(! a < b);
+ console.log(! a < b); 
+ if (a > 18) {
+    console.log("you can vote");
+ }
+
+ if (a < 18) {
+    console.log("you can not vote");
+ }
+
+ let mode = "dark";
+ let color;
+ 
+ if (mode === "dark") {
+    color = "black";
+ }
+
+ if (mode === "light") {
+    color = "white";
+ }
+
+ console.log(color); */
+
+ let aa = 3;
+ if (aa % 2 === 0) {
+    console.log("even")
+ }  else {
+    console.log("odd")
+ }
